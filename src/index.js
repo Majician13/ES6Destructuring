1f289a1529f61ddb72223e3ba948e212a9187631
Majician13 <44847744+Majician13@users.noreply.github.com>
2022-05-04 21:59:14 -0400

    First attempt at Challenge

--- src/index.js
+++ src/index.js
@@ -1,6 +1,24 @@
+// import animals from "./data";
+
+// console.log(animals);
+
+// const [cat, dog] = animals;
+
+// console.log(cat);
+
+// // const {name: catName, sound: catSound} = cat;
+
+// const { name = "Fluffy", sound = "Purr" } = cat;
+
+// console.log(sound);
+
 // CHALLENGE: uncomment the code below and see the car stats rendered
-// import React from "react";
-// import ReactDOM from "react-dom";
+import React from "react";
+import ReactDOM from "react-dom";
+import cars from "./practice";
+
+const [model, coloursByPopularity, speedStats] = cars;
+console.log(speedStats);
 
 // ReactDOM.render(
 //   <table>
